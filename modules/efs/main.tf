eb83c8a6555aa76cd8674f278e31d5e04d2e626b
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-06 16:26:36 +0300

    Added main functionality to the efs module

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -2,7 +2,7 @@
 
 resource "aws_efs_file_system" "wp_efs" {
   creation_token = "wp-efs"
-  perfromance_mode = "${var.efs_performance}"
+  performance_mode = "${var.efs_performance}"
   
   tags = {
     Name = "wp-efs"
@@ -11,10 +11,10 @@ resource "aws_efs_file_system" "wp_efs" {
 
 resource "aws_efs_mount_target" "alpha" {
   file_system_id = "${aws_efs_file_system.wp_efs.id}"
-  subnet_id = "${var.efs_subnets[0]}"
+  subnet_id = "${element(var.efs_subnets, 0)}"
 }
 
 resource "aws_efs_mount_target" "beta" {
   file_system_id = "${aws_efs_file_system.wp_efs.id}"
-  subnet_id = "${var.efs_subnets[1]}"
+  subnet_id = "${element(var.efs_subnets, 1)}"
 }
