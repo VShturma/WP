0fa99258e595d9d45704ed2479e377cd0e1ea1dc
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-06 16:27:48 +0300

    Small update to outputs

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,9 +1,11 @@
 #-----/networking/outputs.tf-----
 
 output "data_subnets" {
-	value = "${aws_subnet.data.*.id}"
+  value = ["${aws_subnet.data.*.id}"]
 }
 
 output "db_sgs" {
-	value = "${aws_security_group.db.id}"
+  value = "${aws_security_group.db.id}"
 }
+
+
