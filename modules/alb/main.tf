0cdf733230b0407740e1aa6cd91bb726e2374987
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-07 13:15:26 +0300

    Added aws_lb resource to the alb module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,2 +1,14 @@
 #-----modules/alb/main.tf-----
 
+resource "aws_lb" "alb_wp" {
+  name = "ALB-WP"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = "${var.alb_sgs}"
+  subnets = "${var.alb_subnets}"
+  ip_address_type = "ipv4"
+
+  tags = {
+    Name = "ALB-WP"
+  }
+}
--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,1 +1,11 @@
 #-----modules/alb/variables.tf-----
+
+variable "alb_sgs" {
+  description = "A list of security group IDs to assign to the ALB"
+  type = "list"
+}
+
+variable "alb_subnets" {
+  description = "A list of subnet IDs to attach to the ALB"
+  type = "list"
+}
