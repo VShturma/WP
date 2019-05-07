c108499bdb0dcf36ded75edf0d8dfdbef54b016f
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-07 15:02:30 +0300

    Fixed error in the alb module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -4,8 +4,8 @@ resource "aws_lb" "alb_wp" {
   name = "ALB-WP"
   internal = false
   load_balancer_type = "application"
-  security_groups = "${var.alb_sgs}"
-  subnets = "${var.alb_subnets}"
+  security_groups = ["${var.alb_sgs}"]
+  subnets = ["${var.alb_subnets}"]
   ip_address_type = "ipv4"
 
   tags = {
