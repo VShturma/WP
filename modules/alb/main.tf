588adaf49a84ec99cf7d62772a6189fe4289c317
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-07 14:51:12 +0300

    Added aws_lb_listener resource to the alb module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -28,3 +28,14 @@ resource "aws_lb_target_group" "alb_tg_public" {
     Name = "PublicAlbTargetGroup"
   }
 }
+
+resource "aws_lb_listener" "front_end_http" {
+  load_balancer_arn = "${aws_lb.alb_wp.arn}"
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = "${aws_lb_target_group.alb_tg_public.arn}"
+  }
+}
