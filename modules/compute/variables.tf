147f6b57b35c86e0c36ade668e89560f948acf24
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-22 17:56:26 +0300

    Added web aws_autoscaling_group resource to the compute module

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -17,3 +17,22 @@ variable "bastion_asg_subnets" {
   description = "List of subnets to assign to the Bastion AutoScaling Group"
   type = "list"
 }
+
+variable "web_sgs" {
+  description = "List of security group IDs to assign to a Web instance"
+  type = "list"
+}
+
+variable "web_user_data" {
+  description = "User data for a Web instance"
+}
+
+variable "web_asg_subnets" {
+  description = "List of subnets to assign to the Web AutoScaling Group"
+  type = "list"
+}
+
+variable "alb_tgs" {
+  description = "A list of target group ARNs, for use with Application Load Balancer"
+  type = "list"
+}
