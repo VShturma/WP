fd701d7197af97bf8d83abdd538739e8033cd0c0
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-23 17:40:17 +0300

    Added private hosted zone config to the DNS module

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,1 +1,10 @@
 #-----modules/dns/main.tf-----
+
+resource "aws_route53_zone" "private" {
+  name = "wp.local"
+  comment = "Private hosted zone for a WordPress environment"
+  
+  vpc {
+    vpc_id = "${var.vpc_id}"
+  }
+}
--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -1,1 +1,6 @@
 #-----modules/dns/variables.tf-----
+
+variable "vpc_id" {
+  description = "The identifier of the VPC in which to create the target group"
+}
+
