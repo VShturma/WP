84ad5f32a071b468226b124ec7da1ef79696b5ac
Volodymyr Shturma <shturma.v@gmail.com>
2019-05-23 17:57:05 +0300

    Added records to the private hosted zone in DNS module

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -8,3 +8,19 @@ resource "aws_route53_zone" "private" {
     vpc_id = "${var.vpc_id}"
   }
 }
+
+resource "aws_route53_record" "fs" {
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name = "fs.wp.local"
+  type = "CNAME"
+  ttl = "300"
+  records = ["${var.fs_endpoint}"]
+}
+
+resource "aws_route53_record" "db" { 
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name = "db.wp.local"
+  type = "CNAME"
+  ttl = "300"
+  records = ["${var.db_endpoint}"]
+}
--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -4,3 +4,10 @@ variable "vpc_id" {
   description = "The identifier of the VPC in which to create the target group"
 }
 
+variable "fs_endpoint" {
+  description = "Elastic File System Endpoint"
+}
+
+variable "db_endpoint" {
+  description = "Database Endpoint"
+}
