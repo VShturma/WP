81d1204ae49d02b861be00ac59efaf62f04e6289
Volodymyr Shturma <shturma.v@gmail.com>
2019-12-12 19:34:05 -0500

    Delete unused IAM role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -4,10 +4,6 @@ data "aws_iam_policy" "ssm_policy" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-data "aws_iam_policy" "cloudwatch" {
-  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
 data "aws_iam_policy_document" "ec2-assume-role-policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -19,79 +15,19 @@ data "aws_iam_policy_document" "ec2-assume-role-policy" {
   }
 }
 
-resource "aws_iam_role" "ssm_role" {
+resource "aws_iam_role" "ssm_instance_role" {
   name = "SSMInstanceRole"
-  description = "Allows EC2 instances to call AWS SSM and Cloudwatch services on your behalf"
+  description = "Allows EC2 instances to call AWS SSM service on your behalf"
   assume_role_policy = data.aws_iam_policy_document.ec2-assume-role-policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_attachment" {
-  role = aws_iam_role.ssm_role.name
+  role = aws_iam_role.ssm_instance_role.name
   policy_arn = data.aws_iam_policy.ssm_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_cloudwatch_attachment" {
-  role = aws_iam_role.ssm_role.name
-  policy_arn = data.aws_iam_policy.cloudwatch.arn
 }
 
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "SSMInstanceProfile"
-  role = aws_iam_role.ssm_role.name
-}
-
-resource "aws_iam_policy" "ssm_send_comand_policy" {
-  name = "SSMSendComandPolicy"
-  
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "ssm:SendCommand",
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:ec2:${var.aws_region}:*:instance/*"
-            ],
-            "Condition": {
-                "StringEquals": {
-                    "ec2:ResourceTag/*": [
-                        "${var.web_instance_name_tag}"
-                    ]
-                }
-            }
-        },
-        {
-            "Action": "ssm:SendCommand",
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:ssm:${var.aws_region}:*:document/AWS-ApplyAnsiblePlaybooks"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-data "aws_iam_policy_document" "events-assume-role-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ssm_send_comand_role" {
-  name = "SSMSendCommandRole"
-  description = "Allows running Ansible Playbooks against EC2 instances"
-  assume_role_policy = data.aws_iam_policy_document.events-assume-role-policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_send_command_attachment" {
-  role = aws_iam_role.ssm_send_comand_role.name
-  policy_arn = aws_iam_policy.ssm_send_comand_policy.arn
+  role = aws_iam_role.ssm_instance_role.name
 }
 
--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -4,12 +4,8 @@ output "ssm_profile" {
   value = aws_iam_instance_profile.ssm_profile
 }
 
-output "ssm_role" {
-  value = aws_iam_role.ssm_role
-}
-
-output "ssm_send_comand_role" {
-  value = aws_iam_role.ssm_send_comand_role
+output "ssm_instance_role" {
+  value = aws_iam_role.ssm_instance_role
 }
 
 
